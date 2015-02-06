3fc8b3818eee73d49bef87de026d7953b0f62ef2
keverino <kmlee1127@gmail.com>
2015-02-06 11:48:59 -0800

    clean up

--- rsa/rsaTest.cpp
+++ rsa/rsaTest.cpp
@@ -1,21 +1,24 @@
 //To compile: g++ -o rsaTest rsaTest.cpp -lcryptopp -pthread
 //To run the program: ./rsaTest
 
-#include "files.h"
-#include "base64.h"
-#include "modes.h"
-#include "osrng.h"
-#include "rsa.h"
-#include "sha.h"
+#include "cryptopp/files.h"
+#include "cryptopp/base64.h"
+#include "cryptopp/modes.h"
+#include "cryptopp/osrng.h"
+#include "cryptopp/rsa.h"
+#include "cryptopp/sha.h"
 #include <string.h>
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 
 using namespace std;
 using namespace CryptoPP;
 //------------------------------------------------------------------------------
 int keyLength;
 string plaintext;
+AutoSeededRandomPool rng;
+InvertibleRSAFunction privkey;
 
 void keyGeneration();
 void sign();
@@ -25,9 +28,10 @@ void print_string_hex(byte*, int);
 //------------------------------------------------------------------------------
 void keyGeneration()
 {
+	// start timer
+	clock_t begin = clock();
+
 	// generate random pool with user defined key length
-	AutoSeededRandomPool rng;
-	InvertibleRSAFunction privkey;
 	privkey.Initialize(rng, keyLength);
 
 	Integer n = privkey.GetModulus();
@@ -36,11 +40,12 @@ void keyGeneration()
 	Integer d = privkey.GetPrivateExponent();
 	Integer e = privkey.GetPublicExponent();
 
-	cout << "n: " << n << endl << endl
-		  << "p: " << p << endl << endl
-		  << "q: " << q << endl << endl
-		  << "d: " << d << endl << endl
-		  << "e: " << e << endl << endl;
+	// output details
+	cout << "[n]: " << n << endl << endl
+		  << "[p]: " << p << endl << endl
+		  << "[q]: " << q << endl << endl
+		  << "[d]: " << d << endl << endl
+		  << "[e]: " << e << endl << endl;
 
 	// save private key to a file.
 	Base64Encoder privkeysink(new FileSink("privkey.txt"));
@@ -52,7 +57,12 @@ void keyGeneration()
 	Base64Encoder pubkeysink(new FileSink("pubkey.txt"));
 	pubkey.DEREncode(pubkeysink);
 	pubkeysink.MessageEnd();
-}
+
+	// stop timer and output time
+	clock_t end = clock();
+	double elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+	cout << "\n[Elapsed Time]: " << elapsedSecs <<  "s" << endl;
+}//end keyGeneration
 //------------------------------------------------------------------------------
 void sign()
 {
@@ -83,7 +93,7 @@ void sign()
 	sink.Put((byte const*) strContents.data(), strContents.size());
 	FileSink sinksig("sig.dat");
 	sinksig.Put(sbbSignature, sbbSignature.size());
-}
+}//end sign()
 //------------------------------------------------------------------------------
 void verify()
 {
@@ -122,27 +132,63 @@ void verify()
 		cout << err.what() << endl;
 	}
 
-}
+}//end verify()
 //------------------------------------------------------------------------------
 void encryption()
 {
 	string cipherText, decryptedText;
+	int trialNumber = 1;
+	double totalTime = 0, elapsedSecs;
+	clock_t begin, end;
+	int counter = 50;
 
-	AutoSeededRandomPool rng;
-	InvertibleRSAFunction params;
-	params.GenerateRandomWithKeySize(rng, keyLength);
-	RSA::PrivateKey private_key(params);
-	RSA::PublicKey public_key(params);
+	cout << "[Plain Text]: " << plaintext << endl << endl;
 
-	RSAES_OAEP_SHA_Encryptor e(public_key);
-	StringSource ss1(plaintext, true, new PK_EncryptorFilter(rng, e, new StringSink(cipherText)));
-	RSAES_OAEP_SHA_Decryptor d(private_key);
-	StringSource ss2(cipherText, true, new PK_DecryptorFilter(rng, d, new StringSink(decryptedText)));
-	cout << "Plain Text: " << plaintext << endl << endl;
-	cout << "Ciphered Text: ";
+	RSA::PrivateKey private_key(privkey);
+	RSA::PublicKey public_key(privkey);
+
+	// encryption
+	cout << "Encrypting 50 times...";
+	while(counter > 0)
+	{
+		cipherText = "";
+		begin = clock();
+		RSAES_OAEP_SHA_Encryptor e(public_key);
+		StringSource ss1(plaintext, true, new PK_EncryptorFilter(rng, e, new StringSink(cipherText)));
+		end = clock();
+		elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+		totalTime += elapsedSecs;
+		counter--;
+		trialNumber++;
+	}
+	cout << "DONE" << endl;
+	cout << "[Elapsed Time]: " << totalTime <<  "s" << endl;
+	cout << "[Ciphered Text]: ";
 	print_string_hex((byte*)cipherText.data(),cipherText.length());
 	cout << endl << endl;
-	cout << "Decrypted Text: " << decryptedText << endl;
+
+	// decryption
+	// reset counters and timer
+	counter = 50;
+	trialNumber = 1;
+	totalTime = 0;
+	elapsedSecs = 0;
+	cout << "Decrypting 50 times...";
+	while(counter > 0)
+	{
+		decryptedText = "";
+		begin = clock();
+		RSAES_OAEP_SHA_Decryptor d(private_key);
+		StringSource ss2(cipherText, true, new PK_DecryptorFilter(rng, d, new StringSink(decryptedText)));
+		end = clock();
+		elapsedSecs = double(end - begin) / CLOCKS_PER_SEC;
+		totalTime += elapsedSecs;
+		counter--;
+		trialNumber++;
+	}
+	cout << "DONE";
+	cout << "\n[Decrypted Text]: " << decryptedText;
+	cout << "\n[Elapsed Time]: " << totalTime << "s" << endl;
 }
 //------------------------------------------------------------------------------
 void print_string_hex(byte* in, int len)
@@ -153,24 +199,24 @@ void print_string_hex(byte* in, int len)
 //------------------------------------------------------------------------------
 int main()
 {
+	cout << "\n------Key Generation------\n";
 	cout << "Please enter a key length: ";
 	cin >> keyLength;
-
-	cout << "\n------Generating keys------\n";
 	keyGeneration();
 
-	cout << "Signing\n";
+	cout << "Signing ";
 	sign();
+	cout << " OK\n";
 
-	cout << "Verifiying\n";
+	cout << "Verify ";
 	verify();
 
+	cout << "\n------Encryption & Decryption------\n";
 	cout << "Please enter plaintext to encrypt: ";
 	cin >> plaintext;
-
-	cout << "\n------Encryption & Decryption------\n";
+	//getline(cin, plaintext);
 	encryption();
 
 	return 0;
-}
+}//end main()
 //------------------------------------------------------------------------------
